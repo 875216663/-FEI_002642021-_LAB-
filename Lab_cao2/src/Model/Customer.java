7e043432164d9e12d552d5b7a2e028b628a63fc0
FEI CAO <122421787+875216663@users.noreply.github.com>
2023-01-29 23:54:04 -0500

    FEI CAO
    lab_2

---
+++ Lab_cao2/src/Model/Customer.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author caofei
+ */
+public class Customer {//创建customer类中的两个成员变量
+     String fullName;
+     int customerId;
+     
+     public Customer(){
+       }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+    
+}
